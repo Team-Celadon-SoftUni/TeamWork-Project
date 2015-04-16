a14ea1a92ac8b7582532b4f175e95b3f3c8b5161
zhanivanov <zhan.ivanovv@gmail.com>
2015-04-16 04:07:30 +0300

    get user's questions

--- SoftUniFAQSystem.Web/Controllers/UserController.cs
+++ SoftUniFAQSystem.Web/Controllers/UserController.cs
@@ -103,6 +103,31 @@
             return this.Ok(bindedUser);
         }
 
+        [HttpGet]
+        [Route("question")]
+        public IHttpActionResult GetUserQuestions()
+        {
+            var currentUserId = this.User.Identity.GetUserId();
+            var user = this.Data.Users.GetById(currentUserId);
+            if (user == null)
+            {
+                return this.BadRequest(Constants.NotLoggedOn);
+            }
+
+            var questions = Data.Questions.All()
+                .Where(q => q.UserId == currentUserId)
+                .Select(q => new QuestionDataModel
+                {
+                    Id = q.Id,
+                    DateOfOpen = q.DateOfOpen,
+                    QuestionState = q.QuestionState,
+                    Title = q.Title,
+                    UserId = q.UserId
+                });
+
+            return Ok(questions);
+        }
+
         [HttpPost]
         [Route("question")]
         public IHttpActionResult PostNewQuestion(QuestionBindingModel question)
